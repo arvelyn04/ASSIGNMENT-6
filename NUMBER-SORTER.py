c5e5e7c64067442e964b4ef90716d7d1a18968c8
ARVELYN RAMOS <ramosarvelyn@gmail.com>
2021-11-26 21:16:45 +0800

    IF ELSE STATEMENT HIGHEST TO LOWEST

---
+++ NUMBER-SORTER.py
@@ -0,0 +1,35 @@
+print("    INPUT 4 NUMBERS ")
+askNumber1 = int(input(" First Number:\t "))
+askNumber2 = int(input(" Second Number:\t "))
+askNumber3 = int(input(" Third Number:\t "))
+askNumber4 = int(input(" Fourth Number:\t "))
+
+def lowest(askNumber1,  askNumber2, askNumber3, askNumber4):
+        return lowest 
+if (askNumber1 <=askNumber2 ) and (askNumber1 <= askNumber3) and (askNumber1 <= askNumber4):
+         lowest =askNumber1
+         print(f"LOWEST NUMBER THAT YOU ENTERED IS: {lowest}\t ")
+elif (askNumber2 <= askNumber1) and (askNumber2 <= askNumber3) and (askNumber2 <= askNumber4):
+         lowest =askNumber2
+         print(f"LOWEST NUMBER THAT YOU ENTERED IS: {lowest}\t ")
+elif (askNumber3 <= askNumber1) and (askNumber3 <= askNumber2) and (askNumber3 <= askNumber4):
+         lowest =askNumber3
+         print(f"LOWEST NUMBER THAT YOU ENETERED IS: {lowest}\t")
+elif (askNumber4 <= askNumber1) and (askNumber4 <= askNumber2) and (askNumber4 <= askNumber3):
+         lowest =askNumber4
+         print(f"LOWEST NUMBER THAT YOU ENETERED IS: {lowest}\t")
+         
+def highest(askNumber1,  askNumber2, askNumber3, askNumber4):
+        return highest 
+if (askNumber1 >=askNumber2 ) and (askNumber1 >= askNumber3) and (askNumber1 >= askNumber4):
+         highest =askNumber1
+         print(f"HIGHEST NUMBER THAT YOU ENETERED IS: {highest}\t ")
+elif (askNumber2 >= askNumber1) and (askNumber2 >= askNumber3) and (askNumber2 >= askNumber4):
+         highest=askNumber2
+         print(f"HIGHEST NUMBER THAT YOU ENTERED IS: {highest}\t ")
+elif (askNumber3 >= askNumber1) and (askNumber3 >= askNumber2) and (askNumber3 >= askNumber4):
+         highest =askNumber3
+         print(f"HIGHEST NUMBER THAT YOU ENERTED IS: {highest}\t")
+elif (askNumber4 >= askNumber1) and (askNumber4 >= askNumber2) and (askNumber4 >= askNumber3):
+         highest=askNumber4
+         print(f"HIGHEST NUMBER THAT YOU ENETERED IS: {highest}\t") 
